e59565762d35ff9b5b8e79d2d69cb2a956f925b4
Evelina Carlsson <evelina.carlsson1@learnet.se>
2024-03-12 11:45:07 +0100

    Framsidan - nästan färdig

---
+++ main.js
@@ -0,0 +1,7 @@
+function slideShow(argId){
+    var image = document.getElementsById(game-image)
+
+    if(argId == "right"){
+        image.setAttribute("src", "https://static.vecteezy.com/ti/gratis-vektor/p3/6409900-tic-tac-toe-skissad-isolerad-vintage-spel-i-handritad-stil-vector.jpg")
+    }
+}
